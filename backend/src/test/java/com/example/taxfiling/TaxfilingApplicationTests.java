d0c89d20da9ecda27433b2307f96e032021f33b4
driedyellowpeach <wrz750726@gmail.com>
2026-03-28 17:28:37 -0700

    feat: basic backend

---
+++ backend/src/test/java/com/example/taxfiling/TaxfilingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taxfiling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxfilingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
