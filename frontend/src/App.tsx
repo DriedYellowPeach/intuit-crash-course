4212e5034a45f55153a2b41ac5fb1fbae01af60c
driedyellowpeach <wrz750726@gmail.com>
2026-03-28 17:31:35 -0700

    feat: basic frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Tax Filing</h1>
+      <p>Intuit Crash Course - Frontend is running.</p>
+    </div>
+  )
+}
+
+export default App
